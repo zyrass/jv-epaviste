0279bc07ba4bf77dfc4b6cc63aae9c5fd45953fd
Alain Guillon <zyrass@outlook.fr>
2024-01-07 19:41:41 +0100

    feature: add a start routing for the client

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,32 @@
 <script setup lang="ts"></script>
 
 <template>
-    <h1>Site en cours de frabrication</h1>
+    <header>
+        <nav>
+            <ul>
+                <routerLink to="/">Accueil</routerLink>
+                <routerLink to="/enlevement-epave">Enlèvement Epave</routerLink>
+                <routerLink to="/formalites-administrative"
+                    >Formalités Administratives</routerLink
+                >
+                <routerLink to="/rachat-metaux">Rachat Métaux</routerLink>
+                <routerLink to="/contact">Contact</routerLink>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Site en cours de frabrication</h1>
+        <hr />
+        <router-view></router-view>
+        <hr />
+    </main>
+    <footer>
+        <span>&copy; Copyright 2024-2027 - tous droits réservés</span>
+        <span
+            >Site réalisé par Mr GUILLON Alain (Développeur fullstack et
+            Alternant RSSI)</span
+        >
+    </footer>
 </template>
 
 <style scoped></style>
--- client/src/router/routes.ts
+++ client/src/router/routes.ts
@@ -1,15 +1,15 @@
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
-import EnlevementEpaves from '../pages/HomePage.vue';
-import Formalites from '../pages/HomePage.vue';
-import RachatMetaux from '../pages/HomePage.vue';
-import Contact from '../pages/HomePage.vue';
-import NotFound from '../pages/HomePage.vue';
+import EnlevementEpaves from '../pages/EnlevementEpaves.vue';
+import Formalites from '../pages/Formalites.vue';
+import RachatMetaux from '../pages/RachatMetaux.vue';
+import Contact from '../pages/Contact.vue';
+import NotFound from '../pages/NotFound.vue';
 
 export const routes: RouteRecordRaw[] = [
     { path: '/', component: HomePage },
     { path: '/enlevement-epave', component: EnlevementEpaves },
-    { path: '/formalite', component: Formalites },
+    { path: '/formalites-administratives', component: Formalites },
     { path: '/rachat-metaux', component: RachatMetaux },
     { path: '/contact', component: Contact },
     { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
